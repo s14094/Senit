69c10320d3dbdc9964313ea1a6e94b06ede7dd69
Kickd <pawel.lakomiec@dahliamatic.pl>
2018-06-09 15:06:50 +0200

    login works

--- Senit/Controller/LoginScreenViewController.swift
+++ Senit/Controller/LoginScreenViewController.swift
@@ -15,160 +15,59 @@ import Alamofire
 
 class LoginScreenViewController: UIViewController {
     
+
     @IBOutlet weak var _username: UITextField!
     @IBOutlet weak var _password: UITextField!
     @IBOutlet weak var _login_button: UIButton!
+    let defaults = UserDefaults.standard
+    let mainVC = ViewController()
     
     
-    // Tuck this away somewhere where it'll be visible to anyone who wants to use it
     var provider = MoyaProvider<AccountApi>()
-    
-
-
     
     override func viewDidLoad() {
         _login_button.layer.cornerRadius = 5
         _login_button.layer.borderWidth = 1
         _login_button.layer.borderColor = UIColor.black.cgColor
-        
-        
-        /*
-        let preferences = UserDefaults.standard
-        
-        if(preferences.object(forKey: "session") != nil)
-        {
-            LoginDone()
-        }
-        else
-        {
-            LoginToDo()
-        }
-        */
     }
     
-    @IBAction func LoginButton(_ sender: Any) {
+    override func viewWillAppear(_ animated: Bool) {
         
-        provider.request(.zen(login: "login",password: "password")) { result in
+    }
+    
+    @IBAction func LoginButton(_ sender: UIButton) {
+        
+        provider.request(.zen(login: _username.text!,password: _password.text!)) { result in
             switch result {
             case let .success(moyaResponse):
                 do {
-                let data = moyaResponse.data // Data, your JSON response is probably in here!
-                let json = try moyaResponse.mapJSON()
-                let statusCode = moyaResponse.statusCode // Int - 200, 401, 500, etc
+                    let data = moyaResponse.data // Data, your JSON response is probably in here!
+                    let json = try moyaResponse.mapJSON()
+                    let statusCode = moyaResponse.statusCode // Int - 200, 401, 500, etc
+                    //print(json)
                     
-                print(data)
-                print(json)
-                print(statusCode)
+                    if (statusCode == 200){
+                        print("zalogowano")
+                        self.defaults.set(true, forKey: "isLogged")
+              
+                        
+                        self.performSegue(withIdentifier: "goToMainMenu", sender: nil)
+                        
+                    }
+                    else {
+                        print("blad logowania: ", statusCode)
+                    }
                 }
                 catch let error {
-                    print(error)
+                    print("error", error)
                 }
-
-
-            // do something in your app
-            case .failure(_): break
-                // TODO: handle the error == best. comment. ever.
+                
+            case .failure(_):
+                print("failure")
+                break
             }
         }
-        
-        /*
-        
-        if(_login_button.titleLabel?.text == "Logout")
-        {
-            let preferences = UserDefaults.standard
-            preferences.removeObject(forKey: "session")
-            
-            LoginToDo()
-            return
-        }
-        
-        
-        let username = _username.text
-        let password = _password.text
-        
-        if(username == "" || password == "")
-        {
-            return
-        }
-        
-        DoLogin(username!, password!)
- 
- */
-    }
- 
-    
-    func DoLogin(_ user:String, _ psw:String)
-    {
-        let url = URL(string: "localhost:8080/api/user/login")
-        let session = URLSession.shared
-        
-        let request = NSMutableURLRequest(url: url!)
-        request.httpMethod = "POST"
-        
-        let paramToSend = "login=" + user + "&password=" + psw
-        
-        request.httpBody = paramToSend.data(using: String.Encoding.utf8)
-        
-        
-        let task = session.dataTask(with: request as URLRequest, completionHandler: {
-            (data, response, error) in
-            
-            guard let _:Data = data else
-            {
-                return
-            }
-            
-            let json:Any?
-            
-            do
-            {
-                json = try JSONSerialization.jsonObject(with: data!, options: [])
-            }
-            catch
-            {
-                return
-            }
-            
-            
-            guard let server_response = json as? NSDictionary else
-            {
-                return
-            }
-            
-            
-            if let data_block = server_response["data"] as? NSDictionary
-            {
-                if let session_data = data_block["session"] as? String
-                {
-                    let preferences = UserDefaults.standard
-                    preferences.set(session_data, forKey: "session")
-                    
-                    DispatchQueue.main.async (
-                        execute:self.LoginDone
-                    )
-                }
-            }
-            
-        })
-        
-        task.resume()
     }
     
-    
-    func LoginToDo()
-    {
-        _username.isEnabled = true
-        _password.isEnabled = true
-        
-        _login_button.setTitle("Login", for: .normal)
-    }
-    
-    func LoginDone()
-    {
-        _username.isEnabled = false
-        _password.isEnabled = false
-        
-        _login_button.setTitle("Logout", for: .normal)
-    }
-    
+   
 }
--- Senit/Controller/ViewController.swift
+++ Senit/Controller/ViewController.swift
@@ -13,13 +13,52 @@ import Moya
 
 class ViewController: UIViewController {
 
-
+    @IBOutlet weak var _registerButton: UIButton!
+    @IBOutlet weak var _loginButton: UIButton!
+    @IBOutlet weak var _userNickName: UIButton!
+    @IBOutlet weak var _logoutButton: UIButton!
+    
+    
+    @IBAction func logoutButton(_ sender: Any) {
+        self.defaults.set(false, forKey: "isLogged")
+        let isLogged = defaults.bool(forKey: "isLogged")
+        print(isLogged)
+        checkLoginValue()
+    }
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkLoginValue()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    
+    func checkLoginValue(){
+        let isLogged = defaults.bool(forKey: "isLogged")
+        if(!isLogged){
+            _loginButton.isEnabled = true
+            _registerButton.isEnabled = true
+            _userNickName.isEnabled = false
+            _logoutButton.isEnabled = false
+            _loginButton.setTitle("Zaloguj", for: .normal)
+            _registerButton.setTitle("Zarejestruj", for: .normal)
+            _userNickName.setTitle(" ", for: .normal)
+            _logoutButton.setTitle(" ", for: .normal)
+        }
+        else {
+            _loginButton.isEnabled = false
+            _registerButton.isEnabled = false
+            _userNickName.isEnabled = true
+            _logoutButton.isEnabled = true
+            _loginButton.setTitle(" ", for: .normal)
+            _registerButton.setTitle(" ", for: .normal)
+            _userNickName.setTitle("dupa", for: .normal)
+            _logoutButton.setTitle("Wyloguj", for: .normal)
+        }
+        print("isLogged: ", isLogged)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
