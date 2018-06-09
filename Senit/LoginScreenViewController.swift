6be5d6d52877a8bfd09bef34a0b0878bd266ec6f
Kickd <pawel.lakomiec@dahliamatic.pl>
2018-06-09 02:13:50 +0200

    something?

--- Senit/LoginScreenViewController.swift
+++ Senit/LoginScreenViewController.swift
@@ -7,29 +7,130 @@
 //
 
 import UIKit
+import SwiftyJSON
+import ObjectMapper
+import Moya
 
 class LoginScreenViewController: UIViewController {
-
+    
+    @IBOutlet weak var _username: UITextField!
+    @IBOutlet weak var _password: UITextField!
+    @IBOutlet weak var _login_button: UIButton!
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        _login_button.layer.cornerRadius = 5
+        _login_button.layer.borderWidth = 1
+        _login_button.layer.borderColor = UIColor.black.cgColor
+        
+        let preferences = UserDefaults.standard
+        
+        if(preferences.object(forKey: "session") != nil)
+        {
+            LoginDone()
+        }
+        else
+        {
+            LoginToDo()
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func LoginButton(_ sender: Any) {
+        
+        if(_login_button.titleLabel?.text == "Logout")
+        {
+            let preferences = UserDefaults.standard
+            preferences.removeObject(forKey: "session")
+            
+            LoginToDo()
+            return
+        }
+        
+        
+        let username = _username.text
+        let password = _password.text
+        
+        if(username == "" || password == "")
+        {
+            return
+        }
+        
+        DoLogin(username!, password!)
     }
-    */
-
+    
+    func DoLogin(_ user:String, _ psw:String)
+    {
+        let url = URL(string: "localhost:8080/api/user")
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url!)
+        request.httpMethod = "POST"
+        
+        let paramToSend = "login=" + user + "&password=" + psw
+        
+        request.httpBody = paramToSend.data(using: String.Encoding.utf8)
+        
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (data, response, error) in
+            
+            guard let _:Data = data else
+            {
+                return
+            }
+            
+            let json:Any?
+            
+            do
+            {
+                json = try JSONSerialization.jsonObject(with: data!, options: [])
+            }
+            catch
+            {
+                return
+            }
+            
+            
+            guard let server_response = json as? NSDictionary else
+            {
+                return
+            }
+            
+            
+            if let data_block = server_response["data"] as? NSDictionary
+            {
+                if let session_data = data_block["session"] as? String
+                {
+                    let preferences = UserDefaults.standard
+                    preferences.set(session_data, forKey: "session")
+                    
+                    DispatchQueue.main.async (
+                        execute:self.LoginDone
+                    )
+                }
+            }
+            
+        })
+        
+        task.resume()
+    }
+    
+    
+    func LoginToDo()
+    {
+        _username.isEnabled = true
+        _password.isEnabled = true
+        
+        _login_button.setTitle("Login", for: .normal)
+    }
+    
+    func LoginDone()
+    {
+        _username.isEnabled = false
+        _password.isEnabled = false
+        
+        _login_button.setTitle("Logout", for: .normal)
+    }
+    
 }
--- Senit/ViewController.swift
+++ Senit/ViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import SwiftyJSON
+import ObjectMapper
+import Moya
 
 class ViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
